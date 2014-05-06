98541775a7f23ac46b67040aa891d1f395469908
Jean-Philippe Camguilhem <jp.camguilhem@gmail.com>
2014-05-06 14:57:51 +0200

    respect lowercase module filenames

---
+++ src/paulla/ircbot/plugins/__init__.py
@@ -0,0 +1,9 @@
+from .chanelloger import ChannelLogger
+from .ykfk import YKFK
+from .paullabot import PaullaBot
+from .politesse import Politesse
+from .taquin import Taquin
+from .urls import Urls
+from .userlist import Userlist
+from .utils import Utils
+
